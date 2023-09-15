835f24c6e9300c45b18bf459efa19e23d42f142b
Robert Zablit <zabrador@users.noreply.github.com>
2023-09-15 22:53:15 +0000

    Add apt, brew, and stow to installation script
    This is primarily intended to automate the installation of `stow` which
    is already used by our automation script (but does not necessarily come
    pre-installed in all environments). In order to do that, we also need to
    detect which package manager to use to install it. For now, brew and apt
    are probably sufficient.
    
    We've also updated the final status message to be more consistent with
    the new messages.

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
 echo "Installing dotfiles..."
+
+if type "brew" > /dev/null; then
+  echo "Using brew for installation..."
+
+  if ! type "stow" > /dev/null; then
+    echo "Installing stow..."
+    brew install stow
+    echo "...stow installation complete!"
+  fi
+elif type "apt" > /dev/null; then
+  echo "Using apt for installation..."
+
+  if ! type "stow" > /dev/null; then
+    echo "Installing stow..."
+    sudo apt install stow
+    echo "...stow installation complete!"
+  fi
+fi
+
 cd ~/.dotfiles
 stow shell
-echo "Installation completed!"
+echo "...dotfiles installation complete!"
